f4366c1090143b974da9c9997095b737ce4ffb78
Daniel Etter <etter.dt@gmail.com>
2018-02-23 18:44:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,11 @@ function logWhisper(string) {
 };
 
 function sayHiToGrandma(string) {
-  return
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!";
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
 };
